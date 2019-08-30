d43520809e856be2804f27f84f214379fa534d06
Nguyen Thanh Tung <nguyenthanhtung@Nguyens-MacBook-Pro.local>
2019-08-30 09:43:43 +0700

    mylap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'ng-starrating';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { Lap02Component } from './lap02/lap02.component';
+import { Lap03Component } from './lap03/lap03.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    Lap02Component,
+    Lap03Component,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    Ng2SearchPipeModule,
+    RatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
